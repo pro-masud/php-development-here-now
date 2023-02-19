b0e3b5919d58bf07f3fba8c560bf203423a08c09
pro-mannan <promasudbd@gmail.com>
2023-02-20 04:00:55 +0600

    add new function here today

---
+++ class 3/index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+// for( $i = $n - 0; $i >= 0; $i--){
+//     //     echo $namberJi [$i]."\n";
+//     // }
+
+//     // // for( $i = 0; $i >= 0; $i--){
+//     // //     echo $namberJi[$i];
+//     // // }
+//     // $fols = ['alo','potol','mango','lemon','dalim','piyara','anaros','licho','boroi'];
+//     // $namberJi = [1,2,3,4,5,6,7,8,9];
+
+//     // $allArrayCount = count($fols);
+//     // $n = count($namberJi);
+
+ // $shift_array = array_shift($folsass,'b');
+    // print_r(  $folsass );
+    // print_r(  $shift_array ) ;
+   
+    $fols = ['alo','potol','mango','lemon','dalim','piyara','anaros','licho','boroi'];
+    $namberJi = [1,2,3,4,5,6,7,8,9];
+    $folsass = [
+        'a' => 'alo',
+        'b' => 'potol',
+        'c' => 'mango',
+        'd' => 'lemon',
+        'e' => 'dalim',
+        'f' => 'piyara',
+        'g' => 'anaros',
+        'h' => 'licho',
+        'i' => 'boroi'
+    ];
+    
+   
+
+
+
+
+
+
+
+
+?>
---
+++ class 4/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Development Class 4 within milligram CSS Framework</title>
+
+    <!-- Milligram Css framework installing here now -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <style>
+        .our_form{
+            margin-top: 60px;
+        }
+    </style>
+</head>
+<body>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="column column-50 column-offset-20">
+                            <div class="our_form">
+                                <form action="">
+                                    <fieldset>
+                                        <label for="fname">Frist Name</label>
+                                        <input name="fname" type="text" id="fname">
+                                        <label for="lname">Last Name</label>
+                                        <input name="lname" type="text"  id="lname">
+                                        <label for="fass">Password</label>
+                                        <input name="pass" type="text" id="fass">
+                                        <input name="submit" class="button-primary" type="submit" value="Send">
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+    
+
+</body>
+</html>
