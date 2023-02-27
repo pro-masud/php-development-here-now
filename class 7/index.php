cc5c2b0dc88d3e1c9969edb1e33d5c1cf39b8705
pro-mannan <promasudbd@gmail.com>
2023-02-27 16:12:39 +0600

    a mode and append mode apply now

--- class 7/index.php
+++ class 7/index.php
@@ -37,19 +37,52 @@
 //    fclose($fileOpen);
 
 
+// w || write mode system here code now
+// if( is_writable($filepath)){
+//     $fileRead = file_get_contents($filepath);
+//     $fileOpen = fopen( $filepath, 'w');
+
+//     fwrite($fileOpen, $fileRead);
+//     echo "\n";
+//     echo "\n";
+//     echo "\n";
+//     fwrite($fileOpen, "Masud rana \n");
+//     fwrite($fileOpen, "PHP \n");
+//     fwrite($fileOpen, "WordPress \n");
+// }
+
+// a || append mode system here code now
+// if( is_writable($filepath)){
+//     $fileRead = file_get_contents($filepath);
+//     $fileOpen = fopen( $filepath, 'w');
+
+//     fwrite($fileOpen, $fileRead);
+//     echo "\n";
+//     echo "\n";
+//     echo "\n";
+//     fwrite($fileOpen, "Masud rana \n");
+//     fwrite($fileOpen, "PHP \n");
+//     fwrite($fileOpen, "WordPress \n");
+// }
+
+
     $filepath = "D:\\PHP\\work\\php development work\\class work\\class 7\\file\\data.txt";
 
-    $fileRead = file_get_contents($filepath);
+    
 
-   $fileOpen = fopen( $filepath, 'a');
+        
+    if( is_writable($filepath)){
+        $fileRead = file_get_contents($filepath);
+        $fileOpen = fopen( $filepath, 'w');
 
-   fwrite($fileOpen, $fileRead);
-   echo "\n";
-   echo "\n";
-   echo "\n";
-   fwrite($fileOpen, "Masud rana \n");
-   fwrite($fileOpen, "PHP \n");
-   fwrite($fileOpen, "WordPress \n");
+        fwrite($fileOpen, $fileRead);
+        echo "\n";
+        echo "\n";
+        echo "\n";
+        fwrite($fileOpen, "Masud rana \n");
+        fwrite($fileOpen, "PHP \n");
+        fwrite($fileOpen, "WordPress \n");
+    }
 
 
    fclose($fileOpen);
