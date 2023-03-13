477157bca4aff9d5a766e84066fd39e36cb7c34b
pro-mannan <promasudbd@gmail.com>
2023-03-13 09:45:27 +0600

    convert a Color code to RGB code this function

--- class 11 OOP/index.php
+++ class 11 OOP/index.php
@@ -1,32 +1,69 @@
 <?php 
 
-    class Human{
+    // class Human{
 
-        public $name; 
+    //     public $name; 
 
-        function humanSay(){
-            echo "my name is masud rana {$this -> name}";
+    //     function humanSay(){
+    //         echo "my name is masud rana {$this -> name}";
+    //     }
+    // }
+
+    // class Cut{
+    //     function catShy(){
+    //         echo "meow";
+    //     }
+    // }
+
+    // $humanObj = new Human();
+    // $humanObj -> name = "masud rana";
+    // $cutObj = new Cut();
+
+
+    // $humanPropartyGetData = $humanObj -> name;
+    // echo $humanPropartyGetData;
+    // // $cutObj -> catShy();
+
+
+    class RGB{
+        private $color;
+        private $red;
+        private $green;
+        private $blue;
+
+        public function __construct($colorCode = '0'){
+
+            $this -> color = ltrim($colorCode,"#");
+
+            $this -> setColor($colorCode );
         }
+        public function getColor(){
+            return $this -> color;
+        }
+        public function setColor($colorCode){
+            $this -> color = ltrim($colorCode,"#");
+            $this -> parSeColor();
+        }
+        private function parSeColor(){
+
+            if($this -> color){
+                sscanf($this -> color, '%02x%02x%02x',$this->red, $this->green,$this->blue);
+                echo $this -> red ."\n";
+                echo $this -> green ."\n";
+                echo $this -> blue ."\n";
+            }else{
+                sscanf($this -> color, '0,0,0');
+            }
+        }
+
     }
 
-    class Cut{
-        function catShy(){
-            echo "meow";
-        }
-    }
-
-    $humanObj = new Human();
-    $humanObj -> name = "masud rana";
-    $cutObj = new Cut();
-
-
-    $humanPropartyGetData = $humanObj -> name;
-    echo $humanPropartyGetData;
-    // $cutObj -> catShy();
-
-
-
-
+    $newRGB = new RGB('#4523af');
+    "\n";
+    "\n";
+    "\n";
+    "\n";
+   
 
 
 
